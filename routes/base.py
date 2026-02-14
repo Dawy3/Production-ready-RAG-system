2cc4162080945be6a6b46555188bb7a38f646f79
Dawy3 <muhamedeldawy586@gmail.com>
2026-02-14 23:02:01 +0200

    .

--- routes/base.py
+++ routes/base.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI, APIRouter
+import os
 
 base_router= APIRouter(
     prefix="/api/v1",
-    
+    tags=["base"]
 )
 
+
 @base_router.get("/")
-def welcome():
+async def welcome():
+    app_name = os.getenv("APP_NAME")
+    app_version = os.getenv("APP_VERSION")
+    
     return{
-        "message" : "Hello, it's working"
+        "app_name" : app_name,
+        "app_version": app_version
     }
