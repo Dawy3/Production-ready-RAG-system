ecc4572a72b7e74a001912218df72e0dd114a592
Dawy3 <muhamedeldawy586@gmail.com>
2026-03-30 23:16:42 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
-from dotenv import load_dotenv
-load_dotenv()
-from src.routes import base
+from src.routes import base, data
 
-app = FastAPI()
+app = FastAPI() 
 
 app.include_router(base.base_router)
+app.include_router(data.data_router)
