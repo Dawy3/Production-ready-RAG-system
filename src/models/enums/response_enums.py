6b682a524f6acf2aafe154c38c5031b9014d34e8
Dawy3 <muhamedeldawy586@gmail.com>
2026-04-04 22:42:33 +0200

    Add DataBaseEnum and update ResponseSignals for consistency

--- src/models/enums/response_enums.py
+++ src/models/enums/response_enums.py
@@ -8,4 +8,5 @@ class ResponseSignals(Enum):
     FILE_UPLOAD_SUCCESS = "file_upload_success"
     FILE_UPLOAD_FAILED = "file_upload_failed"
     PROCESSING_FAILED = "processing_failed"
+    PROCESSING_SUCCESS = "processing_success"
     
