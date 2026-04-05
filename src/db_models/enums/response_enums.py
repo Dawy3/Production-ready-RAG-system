76f01df43ca1c48449863d3e0d7b5198bf1d6a7b
Dawy3 <muhamedeldawy586@gmail.com>
2026-04-05 22:55:45 +0200

    Update enums files

---
+++ src/db_models/enums/response_enums.py
@@ -0,0 +1,16 @@
+"""
+Rather than hard writing every json output message in endpoint. just make enums file (help management and more efficiency way)
+"""
+
+from enum import Enum
+
+class ResponseSignals(Enum):
+    
+    FILE_VALIDATED_SUCCESS = "file_validate_successfully"
+    FILE_TYPE_NOT_SUPPORTED = "file_type_not_supported"
+    FILE_SIZE_EXCEEDED= "file_size_exceeded"
+    FILE_UPLOAD_SUCCESS = "file_upload_success"
+    FILE_UPLOAD_FAILED = "file_upload_failed"
+    PROCESSING_FAILED = "processing_failed"
+    PROCESSING_SUCCESS = "processing_success"
+    
