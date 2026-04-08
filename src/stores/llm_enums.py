47e12db06444e9a78dba7b6253e9c25824e6ce48
Dawy3 <muhamedeldawy586@gmail.com>
2026-04-08 23:11:05 +0200

    Add CoHere enums

--- src/stores/llm_enums.py
+++ src/stores/llm_enums.py
@@ -11,3 +11,15 @@ class OpenAIEnums(Enum):
     USER = "user"
     ASSISTANT = "assistant"
     
+class CohereEnums(Enum):
+    SYSTEM = "SYSTEM"
+    USER = "USER"
+    ASSISTANT = "CHATBOT"
+    
+    DOCUMENT = "search_document"
+    QUERY = "search_query"
+
+class DocumentTypeEnum(Enum):
+    DOCUMENT = "document"
+    QUERY = "query"
+    
