707b75d2ab086280ffaa74eb298c5d1298fe7452
Dawy3 <muhamedeldawy586@gmail.com>
2026-04-13 23:49:11 +0200

    Add RAG answer handling and update response enums for improved error signaling

--- src/routes/nlp_router.py
+++ src/routes/nlp_router.py
@@ -9,7 +9,7 @@ from controllers import NLPController
 
 import logging
 
-
+    
 logger = logging.getLogger('uvicorn.error')
 
 
@@ -128,6 +128,7 @@ async def search_index(request: Request, project_id: str, search_request: Search
         vectordb_client= request.app.vectordb_client,
         generation_client= request.app.generation_client,
         embedding_client= request.app.embedding_client,
+        template_parser = request.app.template_parser,
     )
     
     results = nlp_controller.search_vector_db_collection(
@@ -147,7 +148,47 @@ async def search_index(request: Request, project_id: str, search_request: Search
     return JSONResponse(
         content= {
             "signal" : ResponseSignals.VECTORDB_SEARCH_SUCCESS.value,
-            "result" : results
+            "results" : [result.dict() for result in results]
         }
     )
+    
+
         
+@nlp_router.post("/index/answer/{project_id}")
+async def answer_rag(request: Request, project_id: str, search_request: SearchRequestSchema):
+    
+    project_model = await ProjectModel.create_instance(
+        db_client=request.app.db_client
+    )
+    
+    project = await project_model.get_project_or_create_one(project_id)
+    
+    nlp_controller = NLPController(
+        vectordb_client= request.app.vectordb_client,
+        generation_client= request.app.generation_client,
+        embedding_client= request.app.embedding_client,
+        template_parser = request.app.template_parser,
+    ) 
+    
+    answer, full_prompt, chat_history = nlp_controller.answer_rag_question(
+        project=project,
+        query = search_request.text,
+        limit = search_request.limit,
+    )
+    
+    if not answer:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignals.RAG_ANSWER_ERROR.value
+            }
+        )
+    
+    return JSONResponse(
+            content={
+                "signal": ResponseSignals.RAG_ANSWER_SUCCESS.value,
+                "answer" : answer,
+                "full_prompt": full_prompt,
+                "chat_history" : chat_history
+            }
+        )
