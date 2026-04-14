3f0c66416013d137b36253505ac1a00f6ef9fada
Dawy3 <muhamedeldawy586@gmail.com>
2026-04-14 23:43:58 +0200

    Refactor OpenAIProvider to handle base_url initialization and improve RAG prompt formatting for clarity and consistency

--- src/stores/llm/templates/locales/en/rag.py
+++ src/stores/llm/templates/locales/en/rag.py
@@ -1,35 +1,33 @@
-from string import Template 
- 
-### RAG Template ###
+from string import Template
 
-### System ###
+#### RAG PROMPTS ####
+
+#### System ####
 
 system_prompt = Template("\n".join([
-    "You are an assistant to generate response for the user.",
-    "You will be provided by a set of documents associated with the user's query.",
-    "You have to generatea response based on the documents provided.",
+    "You are an assistant to generate a response for the user.",
+    "You will be provided by a set of docuemnts associated with the user's query.",
+    "You have to generate a response based on the documents provided.",
     "Ignore the documents that are not relevant to the user's query.",
-    "You can applogize to the user if you are nto able to generate response",
+    "You can applogize to the user if you are not able to generate a response.",
     "You have to generate response in the same language as the user's query.",
     "Be polite and respectful to the user.",
-    "Be precise and concise in your response. Avoid unnecessary infromation.",    
+    "Be precise and concise in your response. Avoid unnecessary information.",
 ]))
 
-
-### Document ###
+#### Document ####
 document_prompt = Template(
     "\n".join([
         "## Document No: $doc_num",
         "### Content: $chunk_text",
-]))
-
-### Footer ###
-footer_prompt = Template(
-    "\n".join([
-        "Based only on the above documents, please generate an answer for the user.",
-        "## Question:",
-        "$query",
-        "",
-        "## Answer:",
     ])
 )
+
+#### Footer ####
+footer_prompt = Template("\n".join([
+    "Based only on the above documents, please generate an answer for the user.",
+    "## Question:",
+    "$query",
+    "",
+    "## Answer:",
+]))
