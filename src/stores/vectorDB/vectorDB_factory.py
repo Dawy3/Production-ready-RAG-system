20c70b57b5ad3bd336a52631586edc971e833389
Dawy3 <muhamedeldawy586@gmail.com>
2026-04-19 22:50:08 +0200

    Enhance VectorDBFactory to support PGVectorProvider and refactor QdrantDB initialization

--- src/stores/vectorDB/vectorDB_factory.py
+++ src/stores/vectorDB/vectorDB_factory.py
@@ -1,18 +1,30 @@
-from .providers import QdrantDB
+from .providers import QdrantDB, PGVectorProvider
 from .vectorDB_enums import VectordbEnums
 from controllers.base_controller import BaseController
+from sqlalchemy.orm import sessionmaker
 
 class VectorDBFactory:
-    def __init__(self, config):
+    def __init__(self, config, db_client: sessionmaker = None):
         self.config = config
         self.base_controller = BaseController() 
-        
+        self.db_client = db_client
     def create(self, provider: str):
+        
         if provider == VectordbEnums.QDRANT.value:
-            db_path = self.base_controller.get_database_path(db_name=self.config.VECTOR_DB_PATH)
+            qdrant_db_client = self.base_controller.get_database_path(db_name=self.config.VECTOR_DB_PATH)
             return QdrantDB(
-                db_path= db_path,
-                distance_method = self.config.VECTOR_DB_DISTANCE_METHOD
+                db_client= qdrant_db_client,
+                default_vector_size= self.config.EMBEDDING_MODEL_SIZE,
+                distance_method = self.config.VECTOR_DB_DISTANCE_METHOD,
+                index_threshold= self.config.VECTOR_DB_PGVEC_INDEX_THRESHOLD
+            )
+            
+        if provider == VectordbEnums.PGVECTOR.value:
+            return PGVectorProvider(
+                db_client= self.db_client,
+                default_vector_size= self.config.EMBEDDING_MODEL_SIZE,
+                distance_method= self.config.VECTOR_DB_DISTANCE_METHOD,
+                index_threshold= self.config.VECTOR_DB_PGVEC_INDEX_THRESHOLD
             )
             
         return None
