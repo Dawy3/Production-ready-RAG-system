3a06a0384d3842cc7a10c45b07a59db7a45ff491
Github <github_user@ip-172-26-10-185.ec2.internal>
2026-04-28 17:29:40 +0000

    Update github actions workflow

--- src/main.py
+++ src/main.py
@@ -36,17 +36,17 @@ async def startup_span():
     app.generation_client = llm_provider_factor.create(settings.GENERATION_BACKEND)
     app.generation_client.set_generation_model(settings.GENERATION_MODEL_ID)
     
-    # embedding client
+    # Embedding client
     app.embedding_client = llm_provider_factor.create(settings.EMBEDDING_BACKEND)
     app.embedding_client.set_embedding_model(settings.EMBEDDING_MODEL_ID, settings.EMBEDDING_MODEL_SIZE)
     
-    # vector db client
+    # Vector db client
     app.vectordb_client = vector_db_factory.create(provider=settings.VECTOR_DB_BACKEND)
     await app.vectordb_client.connect()
     
     app.template_parser = TemplateParser(
-        language= settings.PRIMARY_LANG,
-        default_language= settings.DEFAULT_LANG,
+        language=settings.PRIMARY_LANG,
+        default_language=settings.DEFAULT_LANG,
     )
     
     
@@ -58,6 +58,6 @@ app.add_event_handler("startup", startup_span)
 app.add_event_handler("shutdown", shutdown_span)
 
 
-app.include_router(base_router.base_router) # it's route base that check out if the server is working or not 
+app.include_router(base_router.base_router)
 app.include_router(data_router.data_router)
 app.include_router(nlp_router.nlp_router)
